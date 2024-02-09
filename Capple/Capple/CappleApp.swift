066fb2350f70e34722db780349fd2d84c581e973
KyungsooLee <jmtkd9196@gmail.com>
2024-02-09 20:29:15 +0900

    chore: Init Project

---
+++ Capple/Capple/CappleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CappleApp.swift
+//  Capple
+//
+//  Created by Kyungsoo Lee on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CappleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capple/Capple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capple
+//
+//  Created by Kyungsoo Lee on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
