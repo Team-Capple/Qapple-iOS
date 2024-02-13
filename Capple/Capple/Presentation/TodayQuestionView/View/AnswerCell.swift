9e09ada30f78b1a99339d5fe8ba45ae495f1105c
thinkySide <eunlyuing@gmail.com>
2024-02-13 10:37:44 +0900

    feat: TodayQuestionView 화면 구현

---
+++ Capple/Capple/Presentation/TodayQuestionView/View/AnswerCell.swift
@@ -0,0 +1,106 @@
+//
+//  AnswerCell.swift
+//  Capple
+//
+//  Created by 김민준 on 2/12/24.
+//
+
+import SwiftUI
+import FlexView
+
+struct AnswerCell: View {
+    
+    var profileImageText = "튼"
+    var profileName = "튼튼한 당근"
+    
+    var answer = "생각이 깊고 마음이 따뜻한 사람이 좋은 것 같아요. 함께 프로젝트를 진행하며 믿고 의지할 수 있자나요 생각이 깊고 마음이 따뜻한 사람이 좋은 것 같아요."
+    
+    var keywords = ["와플유니버시티", "당근맨", "무자비", "잔망루피", "애플", "아카데미", "디벨로퍼", "디자이너"]
+    
+    var likeCount = 32
+    @State var isLike = false
+    
+    @State var likeColor = GrayScale.secondaryButton
+    
+    var body: some View {
+        
+        /// 프로필
+        HStack(alignment: .top) {
+            Text(profileImageText)
+                .font(.pretendard(.semiBold, size: 18))
+                .foregroundStyle(TextLabel.bk)
+                .frame(width: 28, height: 28)
+                .background(BrandPink.profile)
+                .clipShape(Circle())
+            
+            Spacer()
+                .frame(width: 8)
+            
+            /// 콘텐츠
+            VStack(alignment: .leading) {
+                Text(profileName)
+                    .font(.pretendard(.semiBold, size: 14))
+                    .foregroundStyle(TextLabel.sub2)
+                    .frame(height: 10)
+                    .padding(.top, 8)
+                
+                Spacer()
+                    .frame(height: 12)
+                
+                Text(answer)
+                    .font(.pretendard(.medium, size: 16))
+                    .foregroundStyle(TextLabel.main)
+                    .lineLimit(.max)
+                    .lineSpacing(6)
+                    .multilineTextAlignment(.leading)
+                
+                Spacer()
+                    .frame(height: 12)
+                
+                /// 라이브러리 사용해버렸습니다,, 나중에 공부하면서 수정해보기
+                FlexView(data: keywords, spacing: 8, alignment: .leading) { keyword in
+                    Text("#\(keyword)")
+                        .font(.pretendard(.semiBold, size: 14))
+                        .foregroundStyle(BrandPink.text)
+                }
+                
+                Spacer()
+                    .frame(height: 12)
+                
+                Button {
+                    likeColor = isLike ? GrayScale.secondaryButton : BrandPink.button
+                    isLike.toggle()
+                    // TODO: - 좋아요 탭 기능 구현
+                } label: {
+                    HStack(spacing: 4) {
+                        Image(systemName: "heart.fill")
+                            .frame(width: 24, height: 24)
+                        .foregroundStyle(likeColor)
+                        
+                        Text("\(likeCount)")
+                            .font(.pretendard(.medium, size: 14))
+                            .foregroundStyle(TextLabel.sub3)
+                    }
+                }
+            }
+            
+            /// 아이콘
+            Button {
+                // TODO: - 신고 창 띄우기
+            } label: {
+                Image(systemName: "ellipsis")
+                    .foregroundStyle(TextLabel.sub2)
+                    .frame(width: 20, height: 20)
+            }
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color(Background.first)
+            .ignoresSafeArea()
+        
+        AnswerCell()
+    }
+}
--- Capple/Capple/Presentation/TodayQuestionView/View/TodayQuestionView.swift
+++ Capple/Capple/Presentation/TodayQuestionView/View/TodayQuestionView.swift
@@ -12,7 +12,152 @@ struct TodayQuestionView: View {
     @StateObject var viewModel: TodayQuestionViewModel
     
     var body: some View {
-        Text("캐플캐플")
+        ScrollView {
+            
+            ZStack {
+                Color(Background.first)
+                
+                VStack(spacing: 0) {
+                    WaitingQuestionView()
+                    
+                    TodayQuestionActionButtonView()
+                    
+                    AnswerPreview()
+                }
+            }
+        }
+        .ignoresSafeArea()
+        .background(Background.first)
+    }
+}
+
+// MARK: - WaitingQuestionView
+private struct WaitingQuestionView: View {
+    
+    var makingQuestionText = "오전 질문을 만들고 있어요"
+    var makingSeconds = "03:30:35"
+    
+    fileprivate var body: some View {
+        ZStack {
+            
+            Color(Background.second)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text(makingQuestionText)
+                    .font(.pretendard(.bold, size: 16))
+                    .foregroundStyle(.wh)
+                    .frame(height: 11)
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                Text(makingSeconds)
+                    .font(.pretendard(.bold, size: 38))
+                    .foregroundColor(Color(red: 0.83, green: 0.41, blue: 0.98))
+                    .frame(height: 27)
+                
+                Spacer()
+                    .frame(height: 10)
+                
+                Image(.timer)
+                    .scaledToFit()
+                    .frame(width: 120 , height: 120)
+                
+                Spacer()
+                    .frame(height: 20)
+            }
+        }
+        .frame(height: 348)
+        .ignoresSafeArea()
+    }
+}
+
+// MARK: - TodayQuestionActionButtonView
+private struct TodayQuestionActionButtonView: View {
+    
+    var buttonText = "이전 질문 보러가기"
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            Rectangle()
+                .frame(height: 28)
+                .foregroundStyle(Background.second)
+                .cornerRadius(32, corners: .bottomLeft)
+                .cornerRadius(32, corners: .bottomRight)
+            
+            TodayQuestionActionButton(buttonText)
+        }
+    }
+}
+
+// MARK: - AnswerPreview
+private struct AnswerPreview: View {
+    
+    var questionMark: AttributedString = {
+        var text = AttributedString("Q. ")
+        text.foregroundColor = BrandPink.text
+        return text
+    }()
+    
+    var questionText: AttributedString = "아카데미 러너 중\n가장 마음에 드는 유형이 있나요?"
+    var previewTitle = "이전 답변 좋아요 TOP 3"
+    
+    var answers = [1, 2, 3]
+    
+    fileprivate var body: some View {
+        ZStack(alignment: .leading) {
+            Color(Background.first)
+                .ignoresSafeArea()
+            
+            /// 상단 타이틀
+            VStack(spacing: 14) {
+                VStack(alignment: .leading) {
+                    Spacer()
+                        .frame(height: 44)
+                    
+                    Text(questionMark + questionText)
+                        .font(.pretendard(.bold, size: 20))
+                        .foregroundStyle(TextLabel.main)
+                    
+                    Spacer()
+                        .frame(height: 32)
+                    
+                    HStack {
+                        Text(previewTitle)
+                            .font(.pretendard(.medium, size: 14))
+                            .foregroundStyle(TextLabel.sub3)
+                            .frame(height: 10)
+                        
+                        Spacer()
+                        
+                        SeeAllButton()
+                    }
+                }
+                .padding(.horizontal, 24)
+                
+                Separator()
+                    .padding(.leading, 24)
+                
+                Spacer()
+                
+                ForEach(answers, id: \.self) { _ in
+                    VStack(spacing: 24) {
+                        AnswerCell()
+                            .padding(.horizontal, 24)
+                        
+                        Separator()
+                            .padding(.leading, 24)
+                    }
+                    .padding(.bottom, 16)
+                }
+                
+                Spacer()
+                    .frame(height: 32)
+            }
+        }
     }
 }
 
