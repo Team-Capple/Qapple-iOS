cd5dc328cce9e085e593471f2c30761230f70c74
KyungsooLee <jmtkd9196@gmail.com>
2024-02-22 03:16:43 +0900

    feat: 키보드 변화 감지 패딩 적용

--- Capple/Capple/Presentation/SignInView/View/SignInView.swift
+++ Capple/Capple/Presentation/SignInView/View/SignInView.swift
@@ -8,32 +8,41 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var clickedLoginButton: Bool = false
+    
     var body: some View {
-        ZStack {
-            Color.first
-                .ignoresSafeArea()
-            
-            VStack {
-                Spacer()
+        NavigationStack {
+            ZStack {
+                Color.first
+                    .ignoresSafeArea()
                 
-                // 로고
-                Text("CAPPLE")
-                    .foregroundStyle(Color.main)
-                    .font(.system(size: 56))
-                    .fontWeight(.bold)
-                    .padding(.top, 8)
-                
-                Spacer()
-                
-                // 로그인 버튼
-                Button {
-                    // TODO: 로그인 기능 구현
-                    /// 이미 회원이면 메인화면으로 이동
-                    /// 회원이 아니라면 회원가입으로 이동
+                VStack {
+                    Spacer()
                     
-                } label: {
-                    Image("AppleIDLoginButton")
+                    // 로고
+                    Text("CAPPLE")
+                        .foregroundStyle(Color.main)
+                        .font(.system(size: 56))
+                        .fontWeight(.bold)
+                        .padding(.top, 8)
+                    
+                    Spacer()
+                    
+                    // 로그인 버튼
+                    Button {
+                        // TODO: 로그인 기능 구현
+                        /// 이미 회원이면 메인화면으로 이동
+                        /// 회원이 아니라면 회원가입으로 이동
+                        
+                        clickedLoginButton.toggle()
+                        
+                    } label: {
+                        Image("AppleIDLoginButton")
+                    }
                 }
+            }
+            .navigationDestination(isPresented: $clickedLoginButton) {
+                SignUpNicknameView()
             }
         }
     }
