2ba6c5cc87359fb14a31b64f87597c8341d48b64
KyungsooLee <jmtkd9196@gmail.com>
2024-02-25 01:35:40 +0900

    feat: MyPageView Section 분리 및 구현

--- Capple/Capple/Presentation/MyPageView/View/MyPageView.swift
+++ Capple/Capple/Presentation/MyPageView/View/MyPageView.swift
@@ -8,29 +8,67 @@
 import SwiftUI
 
 struct MyPageView: View {
+    private let sectionInfos: [SectionInfo] = [
+        SectionInfo(
+            sectionTitle: "질문/답변",
+            sectionContents: [
+                "작성한 답변", 
+                "좋아요 한 질문",
+                "좋아요 한 답변"
+            ], 
+            sectionIcons: [
+                "WriteAnswerIcon",
+                "LikeQuestionIcon",
+                "LikeAnswerIcon"]
+        ),
+        SectionInfo(
+            sectionTitle: "문의 및 제보",
+            sectionContents: [
+                "문의하기",
+                "캐플 디스코드",
+                "캐플 인스타",
+                "캐플 오픈채팅방"
+            ],
+            sectionIcons: [
+                "InquiryIcon",
+                "DiscodeIcon",
+                "InstagramIcon",
+                "OpenChatIcon"
+            ]
+        ),
+        SectionInfo(
+            sectionTitle: "계정 관리",
+            sectionContents: [
+                "로그아웃"
+            ],
+            sectionIcons: [
+                "SignOutIcon"
+            ]
+        )
+    ]
+    
     var body: some View {
         ScrollView {
             ZStack(alignment: .topLeading) {
-                Background.first
-                    .ignoresSafeArea()
                 
-                VStack(alignment: .leading, spacing: 24) {
+                VStack(alignment: .leading) {
                     MyProfileSummary()
 
-                    VStack {
-                        Text("질문/답변")
-                            .foregroundStyle(TextLabel.main)
-                            .font(Font.pretendard(.bold, size: 20))
-                            .frame(height: 14)
+                    ForEach(sectionInfos.indices, id: \.self) { index in
+                        MyPageSection(sectionInfo: sectionInfos[index])
                     }
-                    .padding(24)
-                    
                 }
             }
         }
-        .background(Background.first)
+        .background(Background.second)
         
     }
+}
+
+struct SectionInfo {
+    var sectionTitle: String
+    var sectionContents: [String]
+    var sectionIcons: [String]
 }
 
 #Preview {
