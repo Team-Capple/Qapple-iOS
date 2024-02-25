dccc28ede6a82e6dbd8bb85b21383ff6ad073c2c
thinkySide <eunlyuing@gmail.com>
2024-02-25 23:15:39 +0900

    feat: AnswerConfirmView & ViewModel 파일 생성

---
+++ Capple/Capple/Presentation/AnswerConfirmView/View/AnswerConfirmView.swift
@@ -0,0 +1,23 @@
+//
+//  AnswerConfirmView.swift
+//  Capple
+//
+//  Created by 김민준 on 2/25/24.
+//
+
+import SwiftUI
+
+struct AnswerConfirmView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(Background.second)
+                    .ignoresSafeArea()
+            }
+        }
+    }
+}
+
+#Preview {
+    AnswerConfirmView()
+}
---
+++ Capple/Capple/Presentation/AnswerConfirmView/ViewModel/AnswerConfirmViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AnswerConfirmViewModel.swift
+//  Capple
+//
+//  Created by 김민준 on 2/25/24.
+//
+
+import Foundation
+
+final class AnswerConfirmViewModel: ObservableObject {
+    
+}
