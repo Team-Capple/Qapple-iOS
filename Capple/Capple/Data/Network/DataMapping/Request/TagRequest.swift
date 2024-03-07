279eaccfb8cfe6ac4342a2e3bbcff59c157869a5
thinkySide <eunlyuing@gmail.com>
2024-03-07 22:23:58 +0900

    feat: TagRequest, Search 생성

---
+++ Capple/Capple/Data/Network/DataMapping/Request/TagRequest.swift
@@ -0,0 +1,16 @@
+//
+//  TagRequest.swift
+//  Capple
+//
+//  Created by 김민준 on 3/7/24.
+//
+
+import Foundation
+
+class TagRequest {
+    
+    // 태그(키워드) 검색 구조체
+    struct Search {
+        let keyword: String // 검색할 태그(키워드)
+    }
+}
