898341a73fd0080951c9d10ff747a0e6e2356cde
thinkySide <eunlyuing@gmail.com>
2024-03-08 13:23:05 +0900

    feat: AnswerRequest, Response 구조체 생성

---
+++ Capple/Capple/Data/Network/DataMapping/Request/AnswerRequest.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerRequest.swift
+//  Capple
+//
+//  Created by 김민준 on 3/8/24.
+//
+
+import Foundation
+
+class AnswerRequest {
+    
+    /// 특정 질문에 대한 답변 요청 구조체
+    struct AnswerList {
+        let questionId: Int
+        let keyword: String = "" // 검색할 키워드
+        let size: Int = 10 // 조회 할 질문의 개수
+    }
+}
