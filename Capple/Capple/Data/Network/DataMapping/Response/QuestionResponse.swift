51e268dab497745c7cde42a0900a46bde5c0ef88
thinkySide <eunlyuing@gmail.com>
2024-03-09 19:56:24 +0900

    fix: 오류 수정

--- Capple/Capple/Data/Network/DataMapping/Response/QuestionResponse.swift
+++ Capple/Capple/Data/Network/DataMapping/Response/QuestionResponse.swift
@@ -9,28 +9,11 @@ import Foundation
 
 struct QuestionResponse {
     
-    /// 메인 질문 조회 Response
-    struct Questions: Codable {
-        
-        let questionInfos: [QuestionInfos]
-        
-        /// 질문 정보
-        struct QuestionInfos: Codable {
-            let questionId: Int
-            let questionStatus: String
-            let content: String
-            let tag: String?
-            let likeCount: Int?
-            let commentCount: Int?
-        }
-    }
-    
     // 질문 모아보기 조회 Response
     struct Questions: Codable {
         
         let questionInfos: [QuestionsInfos]?
-    
-        
+
         struct QuestionsInfos: Codable,Identifiable {
             var id: Int?
             var questionId: Int?
@@ -40,7 +23,5 @@ struct QuestionResponse {
             var likeCount: Int?
             var commentCount: Int?
         }
-
     }
-
 }
