a14c2df4765f202f5480f847a74f50f12790c786
Hyeonhee <hyunheeshim0103@gmail.com>
2024-03-10 16:07:23 +0900

    timeUpdate

--- Capple/Capple/Presentation/SearchResultView/ViewModel/QuestionViewModel.swift
+++ Capple/Capple/Presentation/SearchResultView/ViewModel/QuestionViewModel.swift
@@ -7,20 +7,6 @@ class QuestionViewModel: ObservableObject {
     
     @Published var questions: [QuestionResponse.Questions.QuestionsInfos] = [] // 모든 질문의 목록입니다.
     @Published var isLoading = false // 데이터 로딩 중인지 여부를 나타냅니다.
-    
-    
-    
-    // MARK: - 타임존 코드 
-    /*
-    var timeZoneFormatted: String {
-           if let timeZone = questions.first?.timeZone {
-               return timeZone == "am" ? "오전 질문" : "오후 질문"
-           } else {
-               return "오전 질문" // 기본값 설정
-           }
-       }
-    */
-    
     @Published var searchQuery = ""
 
     // searchQuery의 변경을 감지하고 필터링을 수행합니다.
@@ -87,52 +73,13 @@ class QuestionViewModel: ObservableObject {
             getQuestions()
         }
     }
-
-
-    
-// MARK: - 이전코드
-    /*
-    
-    // 필요에 따라 추가 콘텐츠를 로드합니다.
-    func loadMoreContentIfNeeded(currentItem item: Question?) {
-        guard let item = item, !isLoading else { return }
-        
-        let thresholdIndex = questions.index(questions.endIndex, offsetBy: -5)
-        if let itemIndex = questions.firstIndex(where: { $0.id == item.id }), itemIndex >= thresholdIndex {
-            loadMoreContent()
-        }
-    }
-    
-    // 실제로 새로운 콘텐츠를 로드하는 메소드입니다.
-    func loadMoreContent() {
-        guard !isLoading else { return }
-        isLoading = true
-        
-        // 새로운 질문들을 로드하는 로직을 구현합니다.
-        // 예시로, 임시 데이터를 생성하여 추가합니다.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            guard let self = self else { return }
-            
-            let newQuestions = (1...10).map { i in
-                Question(
-                    id: 0,
-                    timeZone: .am,
-                    date: Date(),
-                    state: .complete,
-                    title: "오전 질문에 답변 후 모든 내용을 확인해보세요",
-                    keywords: [.init(name: "무자비"), .init(name: "당근맨"), .init(name: "와플대학")],
-                    likes: 38,
-                    comments: 185
-                )
-//                Question(id: self.currentPage * self.pageSize + i, title: "Question \((self.currentPage * self.pageSize) + i)", detail: "This is the detail for question \((self.currentPage * self.pageSize) + i).", tags: ["Tag\(i)", "Tag\(i * 2)"], likes: i * 2, comments: i * 3)
-            }
-            self.questions.append(contentsOf: newQuestions )
-            self.filterQuestions(with: searchQuery)
-            self.isLoading = false
-            self.currentPage += 1
-        }
-    }
-    */
+    // 좋아요 버튼이 눌렸을 때 호출되는 메소드
+       func likeButtonTapped(for question: QuestionResponse.Questions.QuestionsInfos) {
+           guard questions.firstIndex(where: { $0.id == question.id }) != nil else { return }
+           // 해당 질문의 likeCount 증가
+           guard var likes = question.likeCount else { return }
+           likes += 1
+       }
     
     // MARK: - 필터링
     /*
