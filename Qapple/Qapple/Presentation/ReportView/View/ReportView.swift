7c3dd436d00d678b1ecc77cece0dd975d8b72d52
한톨 <eunlyuing@gmail.com>
2024-04-12 18:38:01 +0900

    feat: 신고 기능 남용 시 제재 멘트 추가

--- Qapple/Qapple/Presentation/ReportView/View/ReportView.swift
+++ Qapple/Qapple/Presentation/ReportView/View/ReportView.swift
@@ -47,7 +47,7 @@ struct ReportView: View {
                     backgroundColor: .clear
                 )
                 
-                VStack {
+                VStack(alignment: .leading) {
                     ForEach(Array(reportList.enumerated()), id: \.offset) { index, report in
                         Button {
                             reportType = ReportType.allCases[index]
@@ -60,11 +60,20 @@ struct ReportView: View {
                                 .foregroundStyle(.wh)
                                 .frame(maxWidth: .infinity, alignment: .leading)
                                 .frame(height: 48)
-                                .padding(.leading, 24)
                                 .background(Background.first)
                         }
                     }
+                    
+                    Spacer()
+                        .frame(height: 32)
+                    
+                    Text("* 캐플은 모든 사용자가 안전하고 쾌적한 환경에서 서비스를 이용할 수 있도록 최선을 다하고 있어요. 그러나 이를 악용하여 다른 사용자에게 피해를 주는 경우, 제재가 가해질 수 있어요.")
+                        .font(.pretendard(.regular, size: 14))
+                        .foregroundStyle(TextLabel.sub3)
+                        .lineSpacing(8)
                 }
+                .padding(.horizontal, 24)
+                
                 Spacer()
             }
         }
