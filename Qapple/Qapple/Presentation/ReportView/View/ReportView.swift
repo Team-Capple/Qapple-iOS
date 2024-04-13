b19b8773e1c7d519fc8c74a22399c14e9fe66f4e
한톨 <eunlyuing@gmail.com>
2024-04-13 14:32:21 +0900

    feat: AnswerView 불쾌한 콘텐츠 금지 placeholder 추가feat: AnswerView 불쾌한 콘텐츠 금지 placeholder 추가

--- Qapple/Qapple/Presentation/ReportView/View/ReportView.swift
+++ Qapple/Qapple/Presentation/ReportView/View/ReportView.swift
@@ -67,7 +67,7 @@ struct ReportView: View {
                     Spacer()
                         .frame(height: 32)
                     
-                    Text("* 캐플은 모든 사용자가 안전하고 쾌적한 환경에서 서비스를 이용할 수 있도록 최선을 다하고 있어요. 그러나 이를 악용하여 다른 사용자에게 피해를 주는 경우, 제재가 가해질 수 있어요.")
+                    Text("* 캐플은 모든 사용자가 안전하고 쾌적한 환경에서 서비스를 이용할 수 있도록 최선을 다하고 있어요. 그러나 이를 악용하여 다른 사용자에게 피해를 주는 경우, 제재가 가해질 수 있어요")
                         .font(.pretendard(.regular, size: 14))
                         .foregroundStyle(TextLabel.sub3)
                         .lineSpacing(8)
@@ -93,7 +93,7 @@ struct ReportView: View {
                 pathModel.paths.removeLast()
             }
         } message: {
-            Text("신고한 답변은 블라인드 처리 되며, 관리자 검토 후 조치 될 예정이에요.")
+            Text("신고한 답변은 블라인드 처리 되며, 관리자 검토 후 최대 24시간 이내에 조치 될 예정이에요")
         }
     }
 }
