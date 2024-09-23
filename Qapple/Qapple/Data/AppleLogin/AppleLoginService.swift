bf4f83346c3b39d381e93e60f9f6069efe21f650
thinkyside <eunlyuing@gmail.com>
2024-09-23 10:23:09 +0900

    [#215] 자동 로그인 시 리프레쉬 토큰 발급 로직 추가

--- Qapple/Qapple/Data/AppleLogin/AppleLoginService.swift
+++ Qapple/Qapple/Data/AppleLogin/AppleLoginService.swift
@@ -17,10 +17,21 @@ struct AppleLoginService {
         appleIDProvider.getCredentialState(forUserID: userID ?? "") { (credentialState, error) in
             switch credentialState {
             case .authorized:
-                print("✅ [Auto Login Successed]\n")
-                print("✅ [AccessToken Successed]\n\(String(describing: try? SignInInfo.shared.token(.access)))\n")
-                print("✅ [RefreshToken Successed]\n\(String(describing: try? SignInInfo.shared.token(.refresh)))\n")
-                return completion(true)
+                Task {
+                    do {
+                        let response = try await NetworkManager.refreshToken()
+                        try SignInInfo.shared.createToken(.access, token: response.accessToken)
+                        try SignInInfo.shared.createToken(.refresh, token: response.refreshToken)
+                        
+                        print("✅ [Auto Login Successed]\n")
+                        print("✅ [AccessToken Successed]\n\(String(describing: try? SignInInfo.shared.token(.access)))\n")
+                        print("✅ [RefreshToken Successed]\n\(String(describing: try? SignInInfo.shared.token(.refresh)))\n")
+                        
+                        return completion(true)
+                    } catch {
+                        return completion(false)
+                    }
+                }
                 
             case .revoked, .notFound:
                 print("❌ [Auto Login Failed]\n")
