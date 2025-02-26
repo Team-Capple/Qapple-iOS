4d6af01982b410edb2be00a44adbcd1d698f23cd
mooninbeom <dlsqja567@naver.com>
2025-02-26 13:46:33 +0900

    [#319] 타겟 변경

--- Qapple/QappleTests/QappleTests.swift
+++ Qapple/QappleTests/QappleTests.swift
@@ -2,17 +2,18 @@
 //  QappleTests.swift
 //  QappleTests
 //
-//  Created by 김민준 on 2/23/25.
+//  Created by 문인범 on 2/26/25.
 //
 
 import Testing
 import ComposableArchitecture
-import QappleRepository
+import Foundation
 @testable import Qapple
 
 struct QappleTests {
-    @Test
-    func sampleTest() async throws {
-        
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+
 }
