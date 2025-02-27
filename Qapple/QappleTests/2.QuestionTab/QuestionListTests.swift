59203b6bbbde2c801a2866847e43464b215e1449
thinkyside <eunlyuing@gmail.com>
2025-02-27 11:54:44 +0900

    [#322] QuestionList UnitTest 추가

--- Qapple/QappleTests/2.QuestionTab/QuestionListTests.swift
+++ Qapple/QappleTests/2.QuestionTab/QuestionListTests.swift
@@ -15,9 +15,78 @@ struct QuestionListTests {
     
     let testPaginationInfo = QappleAPI.PaginationInfo(threshold: "", hasNext: false)
     
+    let testQuestionList = [
+        Question(id: 0, content: "", publishedDate: .now, isAnswered: false, isLived: false),
+        Question(id: 1, content: "", publishedDate: .now, isAnswered: false, isLived: false),
+        Question(id: 2, content: "", publishedDate: .now, isAnswered: false, isLived: false)
+    ]
+    
+    @Test("질문리스트 첫 화면 테스트")
+    func onAppear() async throws {
+        let store = TestStore(initialState: .init()) {
+            QuestionListFeature()
+        } withDependencies: {
+            $0.questionRepository.fetchQuestionList = { _ in
+                (testQuestionList, testQuestionList.count, testPaginationInfo)
+            }
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.receive(\.questionListResponse) {
+            $0.questionList = testQuestionList
+            $0.totalCount = testQuestionList.count
+            $0.paginationInfo = testPaginationInfo
+        }
+    }
+    
+    @Test("페이지네이션 테스트")
+    func pagination() async throws {
+        let store = TestStore(initialState: .init()) {
+            QuestionListFeature()
+        } withDependencies: {
+            $0.questionRepository.fetchQuestionList = { _ in
+                (testQuestionList, testQuestionList.count * 2, testPaginationInfo)
+            }
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.send(.pagination)
+        await store.receive(\.paginationResponse) {
+            $0.questionList = testQuestionList + testQuestionList
+            $0.totalCount = testQuestionList.count * 2
+            $0.paginationInfo = testPaginationInfo
+        }
+    }
+    
+    @Test("네트워킹 실패 테스트")
+    func networkingFailed() async throws {
+        let store = TestStore(initialState: .init()) {
+            QuestionListFeature()
+        } withDependencies: {
+            $0.questionRepository.fetchQuestionList = { _ in
+                throw TestError.networkingError
+            }
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.receive(\.networkingFailed) {
+            $0.alert = .failedNetworking(with: TestError.networkingError)
+        }
+        await store.send(.pagination)
+        await store.receive(\.networkingFailed) {
+            $0.alert = .failedNetworking(with: TestError.networkingError)
+        }
+    }
+    
     @Test("답변 여부에 따른 질문 Cell 테스트", arguments: [
         Question(id: 0, content: "", publishedDate: .now, isAnswered: false, isLived: false),
-        Question(id: 1, content: "", publishedDate: .now, isAnswered: true, isLived: false),
+        Question(id: 1, content: "", publishedDate: .now, isAnswered: true, isLived: false)
     ])
     func questionCellTapped(argument: Question) async throws {
         let store = TestStore(initialState: .init()) {
