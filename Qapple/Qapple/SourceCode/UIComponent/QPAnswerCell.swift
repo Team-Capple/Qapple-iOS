e385aa0b8ab48bde35cf54be8471a4eec8f7a0b1
mooninbeom <dlsqja567@naver.com>
2025-05-20 10:51:16 +0900

    [#368] AnswerCell 수정

--- Qapple/Qapple/SourceCode/UIComponent/QPAnswerCell.swift
+++ Qapple/Qapple/SourceCode/UIComponent/QPAnswerCell.swift
@@ -84,12 +84,11 @@ private struct NormalCell: View {
             Content()
                 .padding(.top, 8)
                 .padding(.horizontal, 16)
-                .padding(.bottom, 20)
+                .padding(.bottom, 5)
             
-//            Footer()
-//                .padding(.top, 6)
-//                .padding(.bottom, 20)
-//                .padding(.horizontal, 16)
+            Footer()
+                .padding(.bottom, 20)
+                .padding(.horizontal, 16)
         }
         .background(.first)
     }
@@ -156,11 +155,11 @@ private struct NormalCell: View {
                 likeAction()
             } label: {
                 HStack(spacing: 4) {
-                    Image(true ? .heartActive : .heart)
+                    Image(answer.isLiked ? .heartActive : .heart)
                         .resizable()
                         .frame(width: 18, height: 18)
                     
-                    Text("32")
+                    Text("\(answer.heartCount)")
                         .pretendard(.regular, 13)
                         .foregroundStyle(.sub3)
                 }
@@ -175,7 +174,7 @@ private struct NormalCell: View {
                         .resizable()
                         .frame(width: 15, height: 14)
                     
-                    Text("32")
+                    Text("\(answer.commentCount)")
                         .pretendard(.regular, 13)
                 }
                 .foregroundStyle(.sub3)
@@ -343,7 +342,7 @@ private struct ReportedCell: View {
             publishedDate: .now,
             isReported: false,
             isMine: false,
-            isLiked: false,
+            isLiked: true,
             isResignMember: false,
             commentCount: 1,
             heartCount: 1
@@ -357,7 +356,7 @@ private struct ReportedCell: View {
             publishedDate: .now,
             isReported: true,
             isMine: false,
-            isLiked: false,
+            isLiked: true,
             isResignMember: false,
             commentCount: 1,
             heartCount: 1
